ffc5e9291c6e4d0e29e1b7ccf7f90ceeecf943c7
potesala <110117095+tamago572@users.noreply.github.com>
2024-02-25 05:50:10 +0900

    一応できたが映像がどんどん遅くなる

--- main.cpp
+++ main.cpp
@@ -1,37 +1,91 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <unistd.h>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
 using namespace std;
+
+unsigned int sleep(unsigned int seconds);
 
 int main(int argc, char* argv[])
 {
     std::cout << "Welcome!" << std::endl;
 
-    string imgPath = argv[1];
+    string videoPath = argv[1];
 
-    int height = 36;
-    int weight = 48;
+    cv::VideoCapture cap(videoPath);
+    if (!cap.isOpened()) {
+        return 0;
+    }
 
-    std::cout << "画像ファイル: " << imgPath << std::endl;
+    int max_frame = (int)cap.get(cv::CAP_PROP_FRAME_COUNT);
+    int img_h = (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    int img_w = (int)cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    float fps = cap.get(cv::CAP_PROP_FPS);
 
-    // 画像を読み込む
-    cv::Mat img = cv::imread(imgPath);
+    std::cout << "FPS: " << fps << std::endl;
+    std::cout << "height: " << img_h << std::endl;
+    std::cout << "width: " << img_w << std::endl;
+    std::cout << "max frame: " << max_frame << std::endl;
 
-    cv::Mat img_bin;
-    cv::cvtColor(img, img_bin, cv::COLOR_BGR2GRAY);
+    cv::Mat img;
 
-    // 画像を表示
-    cv::imshow("IeiSenpai", img_bin);
+    // system("afplay /Users/potesala/badapple.mp3&");
 
-    // 任意のキーが押されるまで待つ
-    cv::waitKey(0);
+    // フレームレートを設定（1/30秒）
+    std::chrono::milliseconds frame_duration(1000 / 30);
+
+    for (int i = 0; i < max_frame; i++) {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::cout << "\x1b[38;2;255;255;255m" << "current frame: " << i+1 << std::endl;
+        // //1フレーム分取り出してimgに保持させる
+        cap >> img;
+
+        for (int y = 0; y < img_h; y++) {
+            for (int x = 0; x < img_w; x++) {
+
+                int B = img.at<cv::Vec3b>(y, x)[0];
+                int G = img.at<cv::Vec3b>(y, x)[1];
+                int R = img.at<cv::Vec3b>(y, x)[2];
+
+                std::cout << "\x1b[38;2;" << R << ";" << G << ";" << B << "m" << 0;
+            }
+            std::cout << std::endl;
 
 
-    for (int i = 0; i < height; i++) {
-        for (int i = 0; i < weight; i++) {
-            std::cout << 0;
         }
-        std::cout << std::endl;
+        // 処理終了時間を記録
+        std::cout << "\e[" << img_h+2 << "A" << std::endl;
+        auto end = std::chrono::high_resolution_clock::now();
+        // 処理にかかった時間を計算
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        // 次のフレームまでの待機時間を計算（処理にかかった時間を考慮）
+        auto wait_time = frame_duration - elapsed;
+
+        if(wait_time.count() < 0) {
+            wait_time = std::chrono::milliseconds(0);
+        }
+
+        // 次のフレームまで待機
+        std::this_thread::sleep_for(wait_time);
+
+        // std::cout << duration << "ms" << std::endl;
+
+        // auto start2 = std::chrono::high_resolution_clock::now();
+
+
+
+        // auto end2 = std::chrono::high_resolution_clock::now();
+
+        // auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count();
+
+        // std::cout << std::endl << duration2 << "ms" << std::endl;
+
+
+        // break;
     }
 
     return 0;
