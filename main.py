1ffbf1b2cf6b7e486d699c5f44b6dfa29721f71a
potesala <110117095+tamago572@users.noreply.github.com>
2024-03-19 05:26:14 +0900

    Pythonで作り直し、Issueも解決

---
+++ main.py
@@ -0,0 +1,60 @@
+import time
+import cv2
+import sys
+import os
+import pygame
+import datetime
+import threading
+
+args = sys.argv
+
+videoPath = args[1]
+audio = args[2]
+
+cap = cv2.VideoCapture(videoPath)
+
+if (cap.isOpened() == False):
+    print("Error opening video stream or file")
+    sys.exit()
+
+maxFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+img_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+img_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+ret, img = None, None
+
+
+clock = pygame.time.Clock()
+
+
+pygame.mixer.init()
+pygame.mixer.music.load(audio)
+pygame.mixer.music.play(1)
+
+for i in range(maxFrames):
+    ret, img = cap.read()
+
+    if not ret:
+        print("ERROR!! フレームが読み込まれませんでした！")
+        break
+
+
+    for y in range(img_h):
+        for x in range(img_w):
+            B = img[y, x, 0]
+            G = img[y, x, 1]
+            R = img[y, x, 2]
+
+            text = 0
+            if (B > 36):
+                text = 1;
+
+            print("\x1b[38;2;" + str(R) + ";" + str(G) + ";" + str(B) + "m" + str(text), end="")
+        print("\x1b[0m")
+
+    print(f"frame: {i}")
+
+    print("\033[" + str(img_h+2) + "A")
+
+    clock.tick_busy_loop(fps)
